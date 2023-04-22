225f693f6e3bf807ab065f60f10b6f65c4da5c4f
Joseph Passant <j.passant@liverpool.ac.uk>
2023-04-23 00:31:39 +0100

    Updates comments and docstrings

--- fw_unit_tests.py
+++ fw_unit_tests.py
@@ -1,20 +1,26 @@
-"""Unit tests for Floyd Warshall functions"""
-
+"""Unit tests for Floyd Warshall functions."""
+# Requires the unittest and sys modules to be installed
 import unittest
 import sys
 
+# Requires the following files to be in the same directory:
 from recursive_floyd_warshall import floyd_warshall_recursive
 
+# Define a constant to represent no path between two nodes
+# equal to the maximum integer value of the system
 NO_PATH = sys.maxsize
 
 class TestFloyd(unittest.TestCase):
-    """Test case for floyd function"""
+    """Test case for floyd function."""
 
     def test_floyd_warshall_recursive_1(self):
         """
-        Test case 1
-        4 x 4 matrix defined in given pdf and known output
+        Test case 1.
+        Defines a 4 x 4 matrix and calls floyd_warshall_recursive function
+        passing the matrix as an argument.
+        It then compares the output of the function with the known expected output.
         """
+
         graph = [[0, 7, NO_PATH, 8],
                 [NO_PATH, 0, 5, NO_PATH],
                 [NO_PATH, NO_PATH, 0, 2],
@@ -28,8 +34,10 @@ class TestFloyd(unittest.TestCase):
 
     def test_floyd_warshall_recursive_2(self):
         """
-        Test case 2
-        3x3 matrix
+        Test case 2.
+        Defines a 3x3 matrix and calls floyd_warshall_recursive function
+        passing the matrix as an argument.
+        It then compares the output of the function with the known expected output.
         """
         graph = [[0, 5, NO_PATH],
                  [5, 0, 5],
@@ -42,8 +50,10 @@ class TestFloyd(unittest.TestCase):
 
     def test_floyd_warshall_recursive_3(self):
         """
-        Test case 3
-        5x5 matrix
+        Test case 3.
+        Defines a 5 x 5 matrix and calls floyd_warshall_recursive function
+        passing the matrix as an argument.
+        It then compares the output of the function with the known expected output.
         """
         graph = [[0 , 3, 5, NO_PATH, NO_PATH],
                  [NO_PATH, 0, 3, NO_PATH, NO_PATH],
@@ -60,8 +70,10 @@ class TestFloyd(unittest.TestCase):
 
     def test_floyd_warshall_recursive_4(self):
         """
-        Test case 4
-        10 x 10 matrix
+        Test case 4.
+        Defines a 10 x 10 matrix and calls floyd_warshall_recursive function
+        passing the matrix as an argument.
+        It then compares the output of the function with the known expected output.
         """
         graph = [[0, 3, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 3],
                  [NO_PATH, 0, 3, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH],
@@ -88,9 +100,10 @@ class TestFloyd(unittest.TestCase):
 
     def test_floyd_warshall_recursive_5(self):
         """
-        Test case 5
-        5 x 5 matrix
-        No edge paths
+        Test case 5.
+        Defines a 5 x 5 matrix and calls floyd_warshall_recursive function
+        passing the matrix as an argument.
+        It then compares the output of the function with the known expected output.
         """
         graph = [[0, 3, 3, 3, 3],
                  [NO_PATH, 0, NO_PATH, NO_PATH, NO_PATH],
@@ -107,9 +120,10 @@ class TestFloyd(unittest.TestCase):
 
     def test_floyd_warshall_recursive_6(self):
         '''
-        Test case 6
-        4 x 4 matrix
-        Densely populated
+        Test case 6.
+        Defines a 4 x 4 matrix and calls floyd_warshall_recursive function
+        passing the matrix as an argument.
+        It then compares the output of the function with the known expected output.
         '''
         graph = [[0, 3, NO_PATH, 6],
                  [NO_PATH, 0, 6, 7],
@@ -121,7 +135,6 @@ class TestFloyd(unittest.TestCase):
                     [7, 6, 0, 13],
                     [6, 9, 3, 0]]
         self.assertEqual(result, expected)
-
 
 if __name__=='__main__':
     unittest.main()
