b2b8f0884865f2ab8d09cf643733151d3e552268
Joseph Passant <j.passant@liverpool.ac.uk>
2023-04-22 12:26:14 +0100

    Updated file to take input from recursive_floyd_warshall.py

--- fw_unit_tests.py
+++ fw_unit_tests.py
@@ -1,23 +1,22 @@
-"""Unit tests for floyd warshall functions"""
+"""Unit tests for Floyd Warshall functions"""
 
 import unittest
 import sys
 
-from iterative_floyd_warshall import floyd
-
+from recursive_floyd_warshall import floyd_warshall_recursive
 
 NO_PATH = sys.maxsize
 
 class TestFloyd(unittest.TestCase):
     """Test case for floyd function"""
 
-    def test_floyd(self):
+    def test_floyd_warshall_recursive_1(self):
         """Test case 1"""
         graph = [[0, 7, NO_PATH, 8],
                 [NO_PATH, 0, 5, NO_PATH],
                 [NO_PATH, NO_PATH, 0, 2],
                 [NO_PATH, NO_PATH, NO_PATH, 0]]
-        result = floyd(graph)
+        result = floyd_warshall_recursive(graph)
         expected = [[0, 7, 12, 8],
                     [NO_PATH, 0, 5, 7],
                     [sys.maxsize, sys.maxsize, 0, 2],
