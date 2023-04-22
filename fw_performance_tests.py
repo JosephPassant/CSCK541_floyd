0837a0bcebc9d6ba4ea43bed30acdb2b0ab1f731
Joseph Passant <j.passant@liverpool.ac.uk>
2023-04-22 16:59:24 +0100

    Adapted to calculate performance time for iterative and recursive functions, storing each execution time in a dataframe. Updated test graph to use first test case to reduce test time without affecting sample size as intending to add t-test

--- fw_performance_tests.py
+++ fw_performance_tests.py
@@ -1,29 +1,48 @@
-from recursive_floyd_warshall import floyd_warshall_recursive
-
-import timeit
-
-from fw_unit_tests import TestFloyd 
-
+"""Performace tests for recursive and iterative Floyd Warshall functions"""
 import sys
+import timeit
+import pandas as pd
+from recursive_floyd_warshall import floyd_warshall_recursive
+from iterative_floyd_warshall import floyd_warshall_iterative
+
 NO_PATH = sys.maxsize
 
-graph = [[0, 3, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 3],
-                 [NO_PATH, 0, 3, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH],
-                 [NO_PATH, NO_PATH, 0, 3, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH],
-                 [NO_PATH, NO_PATH, NO_PATH, 0, 3, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH],
-                 [NO_PATH, NO_PATH, NO_PATH, 3, 0, 3, NO_PATH, NO_PATH, NO_PATH, NO_PATH],
-                 [NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0, 3, NO_PATH, NO_PATH, NO_PATH],
-                 [NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0, 3, NO_PATH, NO_PATH],
-                 [NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0, 3, NO_PATH],
-                 [NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0, 3],
-                 [3, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, NO_PATH, 0]]
+graph = [[0, 7, NO_PATH, 8],
+        [NO_PATH, 0, 5, NO_PATH],
+        [NO_PATH, NO_PATH, 0, 2],
+        [NO_PATH, NO_PATH, NO_PATH, 0]]
 
-
-def performance_test():
+def performance_test_recursive():
+    """Performance test for recursive Floyd Warshall function"""
+    start_time = timeit.default_timer()
     floyd_warshall_recursive(graph)
+    return timeit.default_timer() - start_time
+
+df_recursive_results = pd.DataFrame(columns=['Recursive Execution Time (s)'])
+
+for i in range (1,1000):
+    execution_time = performance_test_recursive()
+    #add execution time to dataframe
+    df_recursive_results.loc[i] = [execution_time]
 
 
-execution_time = timeit.timeit(performance_test, number=1000)
+#mean execution time for execution time recursive
+mean_execution_time = df_recursive_results['Recursive Execution Time (s)'].mean()
+print("Mean recursive function execution time: ", mean_execution_time, "(s)")
 
-print("Execution time: ", execution_time, "(s)")
+def performance_test_iterative():
+    """Performance test for iterative Floyd Warshall function"""
+    start_time = timeit.default_timer()
+    floyd_warshall_iterative(graph)
+    return timeit.default_timer() - start_time
 
+df_iterative_results = pd.DataFrame(columns=['Iterative Execution Time (s)'])
+
+for i in range (1,1000):
+    execution_time = performance_test_iterative()
+    #add execution time to dataframe
+    df_iterative_results.loc[i] = [execution_time]
+
+#mean execution time for execution time iterative
+mean_execution_time = df_iterative_results['Iterative Execution Time (s)'].mean()
+print("Mean iterative function execution time: ", mean_execution_time, "(s)")
