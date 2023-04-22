8dc837c306967b3f51c51cae431601fbe9cea7af
Joseph Passant <j.passant@liverpool.ac.uk>
2023-04-22 14:12:07 +0100

    Update removing loop from outer function into inner function. This prevents the need to iteratively call the recursive function and improves performance time for unit test.

--- recursive_floyd_warshall.py
+++ recursive_floyd_warshall.py
@@ -2,28 +2,30 @@
 
 def floyd_warshall_recursive(dist):
     """
-    Outer function
-    Taks a graph as an argument, defines number of vertices
-    iterates for number of intermediate node for all combinations of start and end node 
-    returns minimum distance between nodes
+    This outer function takes a graph as an argument and defines
+    the number of vertices in the graph.
+    It then calls the recursive function shortest_path passing the number of vertices-1 
+    as an argument. This represents the number of intermediate nodes (k).
     """
     vertices = len(dist)
 
-    def shortest_path(i,j,k):
+    def shortest_path(k):
         """
-        defines base case as no intermediate nodes
-        if k ==-1 then the current value is dist is returned
-        else the minimum distance is calculated between the direct path and that using the 
-        intermediate node/s
+        This is the recursive function.
+        It defines the base case as being where there are no intermediate nodes k ==-1.
+        If k ==-1 then the current input matrix is returned.
+        Else the minimum distance is calculated between the direct path betwenn i -> j
+        and that using the intermediate node/s i -> k -> j.
+        K is then decremented and the function is called recursively until k == -1.
         """
         if k == -1:
-            return dist[i][j]
-        else:
-            return min(shortest_path(i, j, k - 1), shortest_path(i, k, k - 1)/
-                        + shortest_path(k, j, k - 1))
-           
-    for k in range (vertices):
-        for i in range (vertices):
+            return dist
+
+        for i in range(vertices):
             for j in range(vertices):
-                dist[i][j] = shortest_path(i, j, k)
-    return dist
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+        k-=1
+        return shortest_path(k)
+
+    return shortest_path(vertices-1)
