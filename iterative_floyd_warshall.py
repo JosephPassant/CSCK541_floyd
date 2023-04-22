0b40182246e97e208d35e18af31892186e768f8d
Joseph Passant <j.passant@liverpool.ac.uk>
2023-04-23 00:28:59 +0100

    Updated to add calling code and add comments

--- iterative_floyd_warshall.py
+++ iterative_floyd_warshall.py
@@ -1,5 +1,16 @@
 """Floyd Warshall Algorithm"""
+import sys
 import itertools
+
+# Define a constant to represent no path between two nodes
+# equal to the maximum integer value of the system
+NO_PATH = sys.maxsize
+
+#Update this graph to test different graphs (must be n x n matrix)
+graph = [[0, 7, NO_PATH, 8],
+        [NO_PATH, 0, 5, NO_PATH],
+        [NO_PATH, NO_PATH, 0, 2],
+        [NO_PATH, NO_PATH, NO_PATH, 0]]
 
 
 def floyd_warshall_iterative(distance):
@@ -12,22 +23,21 @@ def floyd_warshall_iterative(distance):
     """
 
     max_length= len(distance[0])
-    # iterates over all possible combinations of intermediate, start and nodes
 
+    # iterates over all possible combinations of intermediate, start and nodes
     for intermediate, start_node,end_node\
           in itertools.product\
             (range(max_length),range(max_length), range(max_length)):
 
         # if start_node and end_node are the same then the distance between them is set to 0.
-
         if start_node == end_node:
             distance[start_node][end_node] = 0
             continue
 
-        # if start_node and end node are not equal then the following
-        # formula computes the shortest distance between start_node and
-        # end_node by considering all intermediate nodes
-
+        # if the distance between start_node and end_node is greater than
+        # the distance between start_node and intermediate plus the distance
+        # between intermediate and end_node then the distance between
+        # start_node and end_node is updated to the new shorter distance
         distance[start_node][end_node] = min(distance[start_node][end_node],
                         distance[start_node][intermediate]+distance[intermediate][end_node])
 
@@ -36,3 +46,7 @@ def floyd_warshall_iterative(distance):
 
     # prints the updated matrix
     return distance
+
+print("Shortest path matrix using floyd_warshall_iterative function")
+for row in floyd_warshall_iterative(graph):
+    print(row)
