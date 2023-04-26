d0f3a3203605de958abc120e3086334788a10493
Joseph Passant <j.passant@liverpool.ac.uk>
2023-04-26 17:12:38 +0100

    updated range values

--- tests/fw_performance_tests.py
+++ tests/fw_performance_tests.py
@@ -36,7 +36,7 @@ def performance_test(function_name):
 df_recursive_results = pd.DataFrame(columns=['Recursive Execution Time (s)'])
 
 #Run the performance test 1000 times and add execution time to dataframe
-for i in range (0,1000):
+for i in range (1000):
     execution_time = performance_test(floyd_warshall_recursive)
     #Adds execution time to dataframe
     df_recursive_results.loc[i] = [execution_time]
@@ -51,7 +51,7 @@ performance_test(floyd_warshall_iterative)
 df_iterative_results = pd.DataFrame(columns=['Iterative Execution Time (s)'])
 
 #Run the performance test 1000 times and add execution time to dataframe
-for i in range (0,1000):
+for i in range (1000):
     execution_time = performance_test(floyd_warshall_iterative)
     #Add execution time to dataframe
     df_iterative_results.loc[i] = [execution_time]
