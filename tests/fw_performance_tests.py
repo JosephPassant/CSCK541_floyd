6348ba0db3a2adc99187a20e3bcf2ede7f12ba78
Joseph Passant <j.passant@liverpool.ac.uk>
2023-04-28 01:37:23 +0100

    Updated  order of function calls, resolved issue of numpy.objects in data frame impeding t-test execution

--- tests/fw_performance_tests.py
+++ tests/fw_performance_tests.py
@@ -26,7 +26,7 @@ graph = [[0, 7, NO_PATH, 8],
 def performance_test(function_name):
     """
     Performance test for Floyd Warshall functions
-    starts timer, calls function, stops timer and returns execution time
+    starts timer, calls function with graph as argument, stops timer and returns execution time
     """
     start_time = timeit.default_timer()
     function_name(graph)
@@ -40,7 +40,7 @@ def run_performance_test(function_name):
     """
     Runs the performance test 1000 times and adds execution times to the df_results dataframe
     """
-    for i in range (1000):
+    for i in range (200):
         execution_time = performance_test(function_name)
         #Adds execution time to dataframe
         df_results.loc[i, function_name.__name__ + ' execution time (s)'] = execution_time
@@ -53,27 +53,9 @@ mean_iterative_execution_time = df_results['floyd_warshall_iterative execution t
 print("Mean recursive function execution time: ", mean_recursive_execution_time, "(s)")
 print("Mean iterative function execution time: ", mean_iterative_execution_time, "(s)")
 
-def plot_execution_time(col1, col2, title):
-    """
-    Plots the distribution of execution times for two columns on the same chart.
-    df_results is the dataframe containing the execution times.
-    col1 and col2 are the names of the columns to plot.
-    title is the title of the graph.
-    """
-    fig, axis = plt.subplots()
-    axis.hist(df_results[col1], bins=75, label=col1)
-    axis.hist(df_results[col2], bins=75, label=col2)
-    axis.set_title(title)
-    axis.set_xlim(0, 0.00014)
-    axis.set_ylim(0, 1000)
-    axis.set_xlabel('Execution Time (s)')
-    axis.set_ylabel('Frequency')
-    axis.legend()
-    plt.show()
-
-plot_execution_time('floyd_warshall_recursive execution time (s)',\
-                    'floyd_warshall_iterative execution time (s)',\
-                    'Recursive and Iterative Execution Time Distribution')
+# Change all df_results values to numeric values to resolve error in ttest outcome if data\
+# normally distributed
+df_results = df_results.apply(pd.to_numeric)
 
 #Tests for normal distribution of recursive function execution times
 recursive_shapiro_test = shapiro(df_results['floyd_warshall_recursive execution time (s)'])
@@ -91,15 +73,39 @@ if recursive_shapiro_test.pvalue > 0.05 and iterative_shapiro_test.pvalue > 0.05
                             df_results['floyd_warshall_iterative execution time (s)'])
 
     print("p-value: ", pval)
-    print("t-test: ", ttest)
+    print("t-test statistic: ", ttest)
 
     if pval < 0.05:
         print("The difference in mean execution time of the recursive and iterative functions\
-              is significantly significant")
+              is statistically significant")
     else:
         print("The difference in mean execution time of the recursive and iterative functions\
-              is not significantly significant")
+              is not statistically significant")
 
 #If the data is not normally distributed, prints a message to the user
 else:
     print("The data is not normally distributed so it is not appropriate to perform a t-test")
+
+def plot_execution_time(col1, col2, title):
+    """
+    Plots the distribution of execution times for two columns on the same chart.
+    df_results is the dataframe containing the execution times.
+    col1 and col2 are the names of the columns to plot.
+    title is the title of the graph.
+    """
+    fig, axis = plt.subplots()
+    axis.hist(df_results[col1], bins=20, label=col1)
+    axis.hist(df_results[col2], bins=20, label=col2)
+    axis.set_title(title)
+    axis.set_xlim(0, 0.00014)
+    axis.set_ylim(0, 200)
+    axis.set_xlabel('Execution Time (s)')
+    axis.set_ylabel('Frequency')
+    axis.legend()
+    plt.show()
+
+# Plots the distribution of execution times for both functions
+# At end to enable compeltion of script without needing to close the graph
+plot_execution_time('floyd_warshall_recursive execution time (s)',\
+                    'floyd_warshall_iterative execution time (s)',\
+                    'Recursive and Iterative Execution Time Distribution')
